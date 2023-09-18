1b206f9b4a394fc33b9c99af22edbe8e5ac4cae4
alonsovallejo <alonso.garciavallejo@usp.ceu.es>
2023-09-18 11:56:02 +0200

    Ejercicio 7

--- Ejercicio7/Ejercicio7.java
+++ Ejercicio7/Ejercicio7.java
@@ -1,12 +1,19 @@
 package Ejercicio7;
+import java.math.BigInteger;
 import java.util.*;
 public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        int numero=0;
-        int factorial=1;
-        System.out.println("Ingresa el numero a calcular su factorial");
-        n=sc.nextInt();
+        System.out.println("Ingrese el numero: ");
+        BigInteger n = sc.nextBigInteger();
+        sc.close();
+        BigInteger factorial = BigInteger.ONE;
+        BigInteger numero = BigInteger.ONE;
+        while (factorial.compareTo(n)<=0) {
+            numero=numero.add(BigInteger.ONE);
+            factorial=factorial.multiply(numero);
+        }
+        numero=numero.subtract(BigInteger.ONE);
+        System.out.println("El mayor numero cuyo factorial es menor que " + n + "es: "+numero);
     }
 }
